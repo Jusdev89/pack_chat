4cd594e77d88a1f633d767780abcc7d0a69c1b9f
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-03-08 16:36:42 -0800

    Made changes to address PR

--- routes/eventbrite.js
+++ routes/eventbrite.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 import express from 'express'
 const router = express.Router()
 const Eventbrite = require('eventbrite')
 const rp = require('request-promise')
 
-
+const eb_client = Eventbrite({appKey: process.env.EB_APP_KEY, auth: process.env.EB_USER_KEY})
 
 router.get('/users', (request, response) => {
   rp(eb_client)
