342a96fe1747cd6b7748c176efa7524c93f016c7
pllearns@gmail.com <philliplorenzo@Phillips-MacBook-Pro.local>
2017-03-08 19:36:29 -0800

    WIP: Able to get a json response from a get request. Will work on creating all requests before next commit.

--- routes/eventbrite.js
+++ routes/eventbrite.js
@@ -2,13 +2,15 @@ require('dotenv').config()
 import express from 'express'
 const router = express.Router()
 const Eventbrite = require('eventbrite')
-const rp = require('request-promise')
 
-const eb_client = Eventbrite({appKey: process.env.EB_APP_KEY, auth: process.env.EB_USER_KEY})
+const eb_client = Eventbrite({'app_key': process.env.EB_APP_KEY, 'user_key': process.env.EB_USER_KEY})
+console.log('Do I get keys?', eb_client)
 
-router.get('/users', (request, response) => {
-  rp(eb_client)
-  .then(results => {response.json(results)})
-})
+router.get('/', (request, response) => {
+  eb_client.user( {'users': user }, function(err, data){
+    console.log(err);
+    console.log(data);
+    response.json(data)})
+  })
 
 export default router
